fbca0aaf148fc97b36e9850a243679fd3b83ca4f
jmswrnr <jmswrnr@users.noreply.github.com>
2024-06-03 15:47:34 +0100

    refactor: cleanup formatting and styles

--- app/pico8.tsx
+++ app/pico8.tsx
@@ -6,8 +6,6 @@ import { useEffect, useRef, useState } from "react";
 
 import { CartBridge } from "./bridge";
 
-console.log(CartBridge.toString().replace(/^\(\)\w*=>\w*\{?|\}$/g, ""))
-
 const LoadAndPatchPico8Cart = (cart: string) => {
   return URL.createObjectURL(
     new Blob([
@@ -15,9 +13,15 @@ const LoadAndPatchPico8Cart = (cart: string) => {
         CartBridge.toString().replace(/^\(\)\w*=>\w*\{?|\}$/g, ""),
     ])
   );
-}
+};
 
-const Pico8Canvas = ({ cart, resolution }: { cart?: string, resolution: number }) => {
+const Pico8Canvas = ({
+  cart,
+  resolution,
+}: {
+  cart?: string;
+  resolution: number;
+}) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const state = useRef<{
     offscreen: OffscreenCanvas | null;
@@ -39,10 +43,6 @@ const Pico8Canvas = ({ cart, resolution }: { cart?: string, resolution: number }
       { cmd: "init", canvas: state.current.offscreen },
       [state.current.offscreen]
     );
-
-    return () => {
-      // state.current.worker?.terminate()
-    };
   }, [cart]);
 
   return (
@@ -53,26 +53,46 @@ const Pico8Canvas = ({ cart, resolution }: { cart?: string, resolution: number }
       style={{
         width: resolution,
         height: resolution,
-        imageRendering: 'pixelated',
-        background: 'black'
+        imageRendering: "pixelated",
+        background: "black",
       }}
     ></canvas>
   );
 };
 
 export const Pico8Chaos = () => {
-  const [resolution, setResolution] = useState(256);
-  const [instanceCount, setInstanceCount] = useState(1);
+  const [resolution, setResolution] = useState(128);
+  const [instanceCount, setInstanceCount] = useState(32);
   const [cartId, setCartId] = useState<number>(0);
   const [cart, setCart] = useState<string>("");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 gap-4">
-      <header className="flex z-10 w-full gap-2 font-mono text-sm">
-        <div className="flex w-auto justify-center border py-4 px-6 border-neutral-800 bg-zinc-800/30 static rounded-xl items-center font-black">
-          react-pico8-chaos
+      <header className="flex z-10 w-full gap-2 font-mono text-sm whitespace-nowrap  text-slate-400">
+        <div className="flex flex-col w-auto border px-6 py-2 border-neutral-800 bg-zinc-800/30 rounded-xl justify-center items-center gap-1">
+          <strong className="text-white">react-pico8-chaos</strong>
+          <span>
+            by{" "}
+            <a
+              href="https://jmswrnr.com/"
+              className="text-blue-500 font-bold underline"
+              target="_blank"
+            >
+              @jmswrnr
+            </a>
+          </span>
+          <span>
+            source on{" "}
+            <a
+              href="https://github.com/jmswrnr/react-pico8-chaos"
+              className="font-bold underline"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </span>
         </div>
-        <div className="flex flex-col w-auto justify-center border p-4 border-neutral-800 bg-zinc-800/30 static rounded-xl">
+        <div className="flex flex-col w-auto justify-center border px-4 py-2 border-neutral-800 bg-zinc-800/30 static rounded-xl">
           <Label htmlFor="instances" className="mb-2">
             Instances
           </Label>
@@ -82,30 +102,38 @@ export const Pico8Chaos = () => {
             id="instances"
             type="number"
             value={instanceCount}
+            className="text-white"
             onChange={(e) => {
               setInstanceCount(Number(e.target.value));
             }}
           />
         </div>
-        <div className="flex flex-col w-auto justify-center border p-4 border-neutral-800 bg-zinc-800/30 static rounded-xl">
+        <div className="flex flex-col w-auto justify-center border px-4 py-2 border-neutral-800 bg-zinc-800/30 static rounded-xl">
           <Label htmlFor="cartjs" className="mb-2">
             Cart JS File
           </Label>
-          <Input id="cartjs" type="file" onChange={(e) =>{
-            const file = e.target.files?.[0]
-            if (file) {
-              const reader = new FileReader();
-              reader.addEventListener('load', (event) => {
-                if(event.target?.result) {
-                  setCart(LoadAndPatchPico8Cart(event.target.result as string))
-                  setCartId(cartId + 1);
-                }
-              });
-              reader.readAsText(file);
-            }
-          }} />
+          <Input
+            id="cartjs"
+            type="file"
+            className="text-white"
+            onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) {
+                const reader = new FileReader();
+                reader.addEventListener("load", (event) => {
+                  if (event.target?.result) {
+                    setCart(
+                      LoadAndPatchPico8Cart(event.target.result as string)
+                    );
+                    setCartId(cartId + 1);
+                  }
+                });
+                reader.readAsText(file);
+              }
+            }}
+          />
         </div>
-        <div className="flex flex-col w-auto justify-center border p-4 border-neutral-800 bg-zinc-800/30 static rounded-xl">
+        <div className="flex flex-col w-auto justify-center border px-4 py-2 border-neutral-800 bg-zinc-800/30 static rounded-xl">
           <Label htmlFor="resolution" className="mb-2">
             Resolution
           </Label>
@@ -115,18 +143,26 @@ export const Pico8Chaos = () => {
             id="resolution"
             type="number"
             value={resolution}
+            className="text-white"
             onChange={(e) => {
               setResolution(Number(e.target.value));
             }}
           />
         </div>
       </header>
-      <div className="grid grow w-full gap-1" style={{
-        gridTemplateColumns: `repeat(auto-fill, ${resolution}px)`,
-        gridAutoRows: `min-content`
-      }}>
+      <div
+        className="grid grow w-full gap-1"
+        style={{
+          gridTemplateColumns: `repeat(auto-fill, ${resolution}px)`,
+          gridAutoRows: `min-content`,
+        }}
+      >
         {[...Array(instanceCount)].map((_, index) => (
-          <Pico8Canvas key={`${cartId}-${index}`} cart={cart} resolution={resolution} />
+          <Pico8Canvas
+            key={`${cartId}-${index}`}
+            cart={cart}
+            resolution={resolution}
+          />
         ))}
       </div>
     </main>
